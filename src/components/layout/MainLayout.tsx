9415b07f68c76e9e5fd7cbf3ea8380fe88422ad2
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-03-04 12:30:49 +0600

    basic layout implemented

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,76 @@
+import { Layout, Menu, MenuProps } from "antd";
+const { Header, Content, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+   {
+      key: "1",
+      label: "Dashboard",
+   },
+   {
+      key: "2",
+      label: "Profile",
+   },
+   {
+      key: "3",
+      label: "User Management",
+      children: [
+         {
+            key: "3-1",
+            label: "Create Admin",
+         },
+         {
+            key: "3-2",
+            label: "Create Student",
+         },
+      ],
+   },
+];
+
 const MainLayout = () => {
    return (
-      <div>
-         <h1>Main Layout</h1>
-      </div>
+      <Layout style={{ height: "100vh" }}>
+         <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={(broken) => {
+               console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+               console.log(collapsed, type);
+            }}
+         >
+            <div
+               style={{
+                  color: "white",
+                  height: "3.8rem",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+               }}
+            >
+               <h1>PH University</h1>
+            </div>
+            <Menu
+               theme="dark"
+               mode="inline"
+               defaultSelectedKeys={["4"]}
+               items={items}
+            />
+         </Sider>
+         <Layout>
+            <Header style={{ padding: 0 }} />
+            <Content style={{ margin: "24px 16px 0" }}>
+               <div
+                  style={{
+                     padding: 24,
+                     minHeight: 360,
+                  }}
+               >
+                  <h1>Main Content</h1>
+               </div>
+            </Content>
+         </Layout>
+      </Layout>
    );
 };
 
