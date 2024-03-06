dc2b1f0192d381dd4bd10b8b410247b4e44caf58
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-03-06 13:37:32 +0600

    added faculty & student dashboard

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,8 @@ import About from "../pages/About";
 import Login from "../pages/Login";
 import { routeGenerator } from "../utils/routesGenerator";
 import { adminPaths } from "./admin.routes";
+import { facultyPaths } from "./faculty.routes";
+import { studentPaths } from "./student.routes";
 
 const router = createBrowserRouter([
    {
@@ -20,6 +22,16 @@ const router = createBrowserRouter([
       path: "/admin",
       element: <App />,
       children: routeGenerator(adminPaths),
+   },
+   {
+      path: "/faculty",
+      element: <App />,
+      children: routeGenerator(facultyPaths),
+   },
+   {
+      path: "/student",
+      element: <App />,
+      children: routeGenerator(studentPaths),
    },
    {
       path: "/login",
