aaa628036a8efcfc0d7608eb85c96493bf166460
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-03-06 14:02:47 +0600

    modified academic semester

--- src/pages/admin/academicManagment/AcademicSemester.tsx
+++ src/pages/admin/academicManagment/AcademicSemester.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useGetAllAcademicSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
 
 const AcademicSemester = () => {
+   const { data } = useGetAllAcademicSemestersQuery(undefined);
+   console.log(data);
+
    return (
       <div>
          <h1>Academic Semester</h1>
